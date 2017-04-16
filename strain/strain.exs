5109a45af4a0660ce97407fe596add4274f98373
Maciek Rylko <rylko.maciej@gmail.com>
2017-04-16 14:53:15 +0200

    proper solution for strain

--- strain/strain.exs
+++ strain/strain.exs
@@ -6,7 +6,21 @@ defmodule Strain do
   Do not use `Enum.filter`.
   """
   @spec keep(list :: list(any), fun :: ((any) -> boolean)) :: list(any)
-  def keep(list, fun), do: Enum.filter(list, fun)
+  def keep([], _), do: []
+  def keep(list, fun) do
+    keep(list, fun, [])
+  end
+
+  def keep([], _, acc), do: Enum.reverse(acc)
+  def keep([head | tail], fun, acc) do
+    if fun.(head) do
+      keep(tail, fun, [head | acc])
+    else
+      keep(tail, fun, acc)
+    end
+  end
+
+  # def keep(list, fun), do: Enum.fstilter(list, fun)
 
   @doc """
   Given a `list` of items and a function `fun`, return the list of items where
@@ -15,6 +29,6 @@ defmodule Strain do
   Do not use `Enum.reject`.
   """
   @spec discard(list :: list(any), fun :: ((any) -> boolean)) :: list(any)
-  def discard(list, fun), do: Enum.filter(list, &(!fun.(&1)))
-  
+  def discard(list, fun), do: keep(list, &(!fun.(&1)))
+
 end
